fed00b27bfc5740a77b5c9595641d638f5cad754
Oksana Ivashkevych <oksana@bnl.gov>
2019-03-07 23:32:45 -0500

    first commit, reads/write preassure, reads sensor, tweak is not tested

---
+++ iocBoot/iocelveFlowESmobile/st.cmd
@@ -0,0 +1,26 @@
+#!../../bin/windows-x64-static/elveFlowESmobile
+
+#- You may have to change elveFlowESmobile to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/elveFlowESmobile.dbd"
+elveFlowESmobile_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=flowuser")
+dbLoadTemplate("db/elveFlow.substitutions")
+ 
+## Configure port driver # USBelveFlowConfig(portName)        
+# The name to give to this asyn port driver  
+USBelveFlowConfig("elveFlowOB1", 1)
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=flowuser"
