7861511b6ff30108843f2e7333d511c18f966338
Oksana Ivashkevych <oksana@bnl.gov>
2019-03-22 16:52:52 -0400

    added save/restore req files and configuration. Save/restore is working now

--- iocBoot/iocelveFlowESmobile/st.cmd
+++ iocBoot/iocelveFlowESmobile/st.cmd
@@ -22,12 +22,22 @@ USBelveFlowConfig("elveFlowOB1", 1)
 
 #asynSetTraceMask elveFlowOB1 -1 255
 
+set_requestfile_path("./")
+set_requestfile_path($(STD), "stdApp/Db")
+set_requestfile_path($(ELVEFLOW), "elveFlowApp/Db")
+###set_savefile_path("./autosave")
+set_pass0_restoreFile("auto_settings.sav")
+set_pass1_restoreFile("auto_settings.sav")
+
+# specify where save files should be
+set_savefile_path("${TOP}", "autosave")
+
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 # save things every thirty seconds
-epicsEnvSet ("PREFIX", "XF11ID-ES\{elveFlowOB1\}:")
-create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
+
+create_monitor_set("auto_settings.req", 30)
 
 ## Start any sequence programs
 #seq sncxxx,"user=flowuser"
